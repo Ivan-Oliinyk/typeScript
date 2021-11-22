9ea697dfe8a95f61b5b9a1fee26acde28e2757cc
Ivan <lorf1991@gmail.com>
2021-11-22 19:00:14 +0200

    ad

--- dist/index.js
+++ dist/index.js
@@ -14,6 +14,27 @@ class myString extends String {
     }
 }
 const str = new myString('Hey ').repeatMy(5);
-console.log(str);
-/////////////////////////////////
+// console.log(str)
+/////////////////////////////////myIndex\\\\\\\
+class myIndexOf extends String {
+    indexOf(str, pos = 0) {
+        if (pos < 0 || typeof pos !== "number") {
+            pos = 0;
+        }
+        if (pos > this.length) {
+            return -1;
+        }
+        for (let i = pos; i < this.length; i++) {
+            if (this.slice(i, str.length + i) === str) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+const index = new myIndexOf('poly12o3doly');
+// console.log(index.indexOf('ly'))
+// console.log(index.indexOf('123'))
+console.log(index.indexOf('o', 2));
+////////////////myfilter //////////////////////////////
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -18,10 +18,36 @@ class myString extends String {
 }
 
 const str: string = new myString('Hey ').repeatMy(5)
-console.log(str)
+// console.log(str)
 
 
-/////////////////////////////////
+/////////////////////////////////myIndex\\\\\\\
+class myIndexOf extends String {
+  indexOf(str: string, pos: number = 0): number {
+    if(pos < 0 || typeof pos !== "number") {
+      pos = 0
+    }
+    
+    if (pos > this.length) {
+      return -1
+    }
+
+    for (let i = pos; i < this.length; i++) {
+      if(this.slice(i, str.length + i) === str) {
+        return i
+      }
+    }
+
+    return -1
+  }
+}
+
+const index: myIndexOf = new myIndexOf('poly12o3doly')
+// console.log(index.indexOf('ly'))
+// console.log(index.indexOf('123'))
+console.log(index.indexOf('o', 2))
+
+////////////////myfilter //////////////////////////////
 
 
 
